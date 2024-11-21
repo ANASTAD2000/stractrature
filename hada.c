d8b8531221ba6f55b9593e8d2632279a4034f659
ANASTAD2000 <anasbaclibr@gmai.com>
2024-11-21 11:35:45 +0100

    like

---
+++ hada.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// i didn't get to structres so i just did this 
+// i only have 1h
+
+int main(){
+    char name[30];
+    int age;
+    char ville[30];
+    char rue[30];
+    int code, n;
+    for(int i = 0; i > n; i++){
+        printf("how many persone you want : ");
+        scanf("%d", &n);
+    printf("enter the name : ");
+    scanf("%s", name);
+        printf("enter the age : ");
+    scanf("%d", &age);
+    printf("\nenter the your address\n: ");
+    printf("enter the rue : ");
+    scanf("%s", rue);
+    printf("enter the ville : ");
+    scanf("%s", ville);
+    printf("enter the code postale : ");
+    scanf("%s", &code);
+    } 
+}
